9f17ebccb35447c1bccc8721279962be8823e91d
VapourDrive <colin.g.bos@gmail.com>
2016-01-28 17:58:46 -0500

    GenLoaderAPI update

--- src/api/java/vapourdrive/genloader/api/serializeable/WeightedBlockState.java
+++ src/api/java/vapourdrive/genloader/api/serializeable/WeightedBlockState.java
@@ -1,16 +1,12 @@
 package vapourdrive.genloader.api.serializeable;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
 
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraftforge.fml.common.registry.GameData;
+import vapourdrive.genloader.api.utils.BlockUtils;
 
-import com.google.common.collect.ImmutableMap;
-
-public class WeightedBlockState
+public class WeightedBlockState implements IWeightedBlockState
 {
 	private final int weight;
 	private final String block;
@@ -21,15 +17,7 @@ public class WeightedBlockState
 	{
 		this.weight = Weight;
 		this.block = GameData.getBlockRegistry().getNameForObject(State.getBlock()).toString();
-		ImmutableMap<IProperty, Comparable> stateProps = State.getProperties();
-		Iterator<Entry<IProperty, Comparable>> propIterater = stateProps.entrySet().iterator();
-		HashMap<String, String> statePropsMap = new HashMap<String, String>();
-		while(propIterater.hasNext())
-		{
-			Entry<IProperty, Comparable> entry = propIterater.next();
-			statePropsMap.put(entry.getKey().getName(), State.getValue(entry.getKey()).toString());
-		}
-		this.properties = statePropsMap;
+		this.properties = BlockUtils.generateProperties(State);
 		this.state = State;
 	}
 
--- src/main/java/vapourdrive/resourcefoundation/world/GenLoaderCompat.java
+++ src/main/java/vapourdrive/resourcefoundation/world/GenLoaderCompat.java
@@ -14,18 +14,13 @@ public class GenLoaderCompat
 	public static void init()
 	{
 		GenerationCategory Category = new GenerationCategory(ModInfo.ModID);
-		addGenerator(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 10, 40, 75, 8, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.COPPER))});
-		addGenerator(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 8, 20, 55, 8, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.TIN))});
-		addGenerator(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 3, 30, 60, 10, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.ALUMINUM))});
-		addGenerator(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 6, 5, 30, 8, new WeightedBlockState[]{new WeightedBlockState(75, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.SILVER)), new WeightedBlockState(25, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.LEAD))});
-		addGenerator(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 8, 10, 35, 8, new WeightedBlockState[]{new WeightedBlockState(15, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.SILVER)), new WeightedBlockState(85, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.LEAD))});
-		addGenerator(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 2, 5, 30, 6, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.PLATINUM)), new WeightedBlockState(60, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.NICKEL))});
+		GenLoaderAPI.addGeneration(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 10, 40, 75, 8, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.COPPER))});
+		GenLoaderAPI.addGeneration(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 8, 20, 55, 8, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.TIN))});
+		GenLoaderAPI.addGeneration(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 3, 30, 60, 10, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.ALUMINUM))});
+		GenLoaderAPI.addGeneration(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 6, 5, 30, 8, new WeightedBlockState[]{new WeightedBlockState(75, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.SILVER)), new WeightedBlockState(25, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.LEAD))});
+		GenLoaderAPI.addGeneration(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 8, 10, 35, 8, new WeightedBlockState[]{new WeightedBlockState(15, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.SILVER)), new WeightedBlockState(85, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.LEAD))});
+		GenLoaderAPI.addGeneration(Category, EnumGenerationType.STANDARDVARIABLECLUSTER, 2, 5, 30, 6, new WeightedBlockState[]{new WeightedBlockState(10, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.PLATINUM)), new WeightedBlockState(60, Mod_Blocks.BlockOre.getDefaultState().withProperty(BlockProperties.ORE_VARIANT, OreVariant.NICKEL))});
 		
 		GenLoaderAPI.addValuableBlock(Mod_Blocks.BlockOre);
-	}
-
-	private static void addGenerator(GenerationCategory Category, EnumGenerationType generation, int frequency, int minY, int maxY, int size, WeightedBlockState[] weightedBlockStates)
-	{
-		GenLoaderAPI.addGeneration(Category, generation, frequency, minY, maxY, size, weightedBlockStates);
 	}
 }
